b6d4923b7c65f906dce679657bb8cdd2cfa32c18
rinvi <rin.vitae@gmail.com>
2022-09-26 00:59:34 -0700

    add new card images, removed some opponent cards, lowered top opponent cards by 100dp

---
+++ app/src/main/java/com/example/crazyeights/MainActivity 2.java
@@ -0,0 +1,19 @@
+package com.example.crazyeights;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Bitmap cardBack = BitmapFactory.decodeResource(getResources(), R.drawable.backOfCard);
+        //canvas.
+
+    }
+}
